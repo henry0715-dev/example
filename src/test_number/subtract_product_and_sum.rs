91288175f5fcee735c476c8595c4d3d445ae73c7
henry0715-dev <henry@cluml.com>
2024-07-17 15:30:13 +0900

    Problem solving - numbers
    ### Info
    
    test numbers
    - minimum_sum
    - num_identical_pairs
    - kids_with_candies
    - subtract_product_and_sum
    - smaller_numbers_than_current

---
+++ src/test_number/subtract_product_and_sum.rs
@@ -0,0 +1,25 @@
+/*
+    https://leetcode.com/problems/subtract-the-product-and-sum-of-digits-of-an-integer/
+    1. 숫자를 배열로 변환 작업
+    2. 배열 곱셈 구하기
+    3. 배열 합 구하기
+ */
+pub fn test() {
+    println!("{}", subtract_product_and_sum(234));
+    println!("{}", subtract_product_and_sum(4421));
+}
+
+fn subtract_product_and_sum(n: i32) -> i32 {
+    let n_array: Vec<i32> = get_array_by_num(n);
+    let product: i32 = n_array.iter().product();
+    let sum: i32 = n_array.iter().sum();
+
+    product - sum
+}
+
+fn get_array_by_num(num: i32) -> Vec<i32> {
+    num.to_string()
+        .chars()
+        .map(|c| c.to_digit(10).unwrap() as i32 )
+        .collect()
+}
