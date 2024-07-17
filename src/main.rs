a0e2acb7b39d59427650348e1e6e91bc8ba096af
henry0715-dev <henry@cluml.com>
2024-07-17 09:19:27 +0900

    revert commit

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,3 @@
-mod test_first;
-mod test_vector;
-
 fn main() {
-
-    test_first::add_two_integers::test();
-
-    // test - Vector
-    test_vector::get_concatenation::test();
-    test_vector::build_array::test();
-    test_vector::running_sum::test();
-    test_vector::maximum_wealth::test();
-    test_vector::shuffle::test();
-
-    // TODO test - Numbers
-    // TODO test - String
-    // TODO test - Encoding/Decoding
-    // TODO test - Iterations
-
-
+    println!("Hello, world!");
 }
--- src/test_vector/mod.rs
+++
@@ -1,5 +0,0 @@
-pub mod get_concatenation;
-pub mod build_array;
-pub mod running_sum;
-pub mod maximum_wealth;
-pub mod shuffle;
