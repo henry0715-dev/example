fad4388f16959747ba5eb177c85c1f239ba31a65
henry0715-dev <henry@cluml.com>
2024-07-23 13:14:15 +0900

    Fix - PR
    - minimum_sum
    - subtract_product_and_sum

--- src/test_number/minimum_sum.rs
+++ src/test_number/minimum_sum.rs
@@ -7,11 +7,26 @@
 */
 
 pub fn test() {
-    println!("minimum_sum result : {:?}", minimum_sum(2932));
-    println!("minimum_sum result : {:?}", minimum_sum(4009));
+    // println!("minimum_sum result : {:?}", minimum_sum(2932));
+    // println!("minimum_sum result : {:?}", minimum_sum(4009));
+    match minimum_sum(123) {
+        None => {
+            println!(
+                "Invalid parameter: Input `num` value should be within a 4-digit number range."
+            );
+        }
+        Some(result) => {
+            println!("minimum_sum result : {result}.");
+        }
+    }
 }
 
 fn minimum_sum(num: i32) -> Option<i32> {
+    //주어진 조건은 4자리 숫자이기 때문에 해당 조건이 맞지 않으면 None 반환
+    if !(1000..=9999).contains(&num) {
+        return None;
+    }
+
     let char_array = get_ordered_char_array_by_nums(num);
     let num1: i32 = format!("{}{}", char_array[0], char_array[2]).parse().ok()?;
     let num2: i32 = format!("{}{}", char_array[1], char_array[3]).parse().ok()?;
@@ -31,4 +46,7 @@ fn tc() {
     let result = minimum_sum(2932);
     let check = 52;
     assert_eq!(result, Some(check));
+
+    let result = minimum_sum(123);
+    assert_eq!(result, None);
 }
--- src/test_number/subtract_product_and_sum.rs
+++ src/test_number/subtract_product_and_sum.rs
@@ -6,33 +6,48 @@
 */
 pub fn test() {
     println!(
-        "subtract_product_and_sum result : {}",
+        "subtract_product_and_sum result : {:?}",
         subtract_product_and_sum(234)
     );
     println!(
-        "subtract_product_and_sum result : {}",
+        "subtract_product_and_sum result : {:?}",
         subtract_product_and_sum(4421)
     );
+
+    match subtract_product_and_sum(-234) {
+        None => {
+            println!("Invalid parameter: The `n` values provided should be positive integers.");
+        }
+        Some(value) => {
+            println!("subtract_product_and_sum result : {value}");
+        }
+    }
 }
 
-fn subtract_product_and_sum(n: u32) -> u32 {
-    let n_array: Vec<u32> = get_array_by_num(n);
-    let product: u32 = n_array.iter().product();
-    let sum: u32 = n_array.iter().sum();
+fn subtract_product_and_sum(n: i32) -> Option<u32> {
+    get_array_by_num(n).and_then(|array| {
+        let product: u32 = array.iter().product();
+        let sum: u32 = array.iter().sum();
 
-    product - sum
+        Option::from(product - sum)
+    })
 }
 
-fn get_array_by_num(num: u32) -> Vec<u32> {
-    num.to_string()
-        .chars()
-        .filter_map(|c| c.to_digit(10))
-        .collect()
+/*
+   i32로 타입을 변경한 이유는 `None` 이 발생할 수 있는 테스트를 위함.
+   * u32에서는 filter_map 사용해도 무방함. : u32로 들어온 경우 c.to_digit(10) 실행시 None 이 되는 경우는 없음.
+     - radix 값이 너무 크게 주어진다면 None 이 발생할 수 있지만, 10으로 고정한 상태임.
+*/
+fn get_array_by_num(num: i32) -> Option<Vec<u32>> {
+    num.to_string().chars().map(|c| c.to_digit(10)).collect()
 }
 
 #[test]
 fn tc() {
     let result = subtract_product_and_sum(234);
     let check = 15;
-    assert_eq!(result, check);
+    assert_eq!(result, Some(check));
+
+    let result = subtract_product_and_sum(-234);
+    assert_eq!(result, None);
 }
