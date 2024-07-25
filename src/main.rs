c6136aa07dd784249c17606c6028d327f5221b7e
henry0715-dev <henry@cluml.com>
2024-07-25 15:58:51 +0900

    Fix - PR
    - cargo fmt -- --check --config group_imports=StdExternalCrate

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,6 @@ mod test_first;
 mod test_vector;
 
 fn main() {
-
     test_first::add_two_integers::test();
 
     // test - Vector
@@ -11,11 +10,4 @@ fn main() {
     test_vector::running_sum::test();
     test_vector::maximum_wealth::test();
     test_vector::shuffle::test();
-
-    // TODO test - Numbers
-    // TODO test - String
-    // TODO test - Encoding/Decoding
-    // TODO test - Iterations
-
-
 }
